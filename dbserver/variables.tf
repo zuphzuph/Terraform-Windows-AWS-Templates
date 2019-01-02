acfb06aecaa02acc2f5d0a8fe3f8d1d7adf9f8b8
zuphzuph <zuphzuph@users.noreply.github.com>
2019-01-02 11:35:17 -0700

    -_-

---
+++ dbserver/variables.tf
@@ -0,0 +1,19 @@
+variable "admin_password" {
+  description = "Windows Administrator password to login as."
+  default = "password"
+}
+
+variable "key_name" {
+  description = "Name of the SSH keypair to use in AWS."
+  default = "key-name-here"
+}
+
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default     = "us-XXXX-#a"
+}
+
+variable "aws_availzone" {
+  description = "AWS availibility zone to launch in."
+  default     = "us-XXXX-#a"
+}
