ff6b5ad47cba439ef2f9909ce1a1e0681c7706e6
zuphzuph <zuphzuph@users.noreply.github.com>
2019-03-01 13:55:58 -0700

    Update main.tf
    ¯\_(ツ)_/¯

--- webserver/main.tf
+++ webserver/main.tf
@@ -56,16 +56,7 @@ resource "aws_instance" "winrm" {
   #
   key_name = "${var.key_name}"
 
-  # Note that terraform uses Go WinRM which doesn't support https at this time. If server is not on a private network,
-  # recommend bootstraping Chef via user_data.  See asg_user_data.tpl for an example on how to do that.
-  # Strip anything you don't need/want below. 
-  # Steps below are as follows:
-  # Adds WinRM Rule
-  # Installs Choco | Chrome
-  # Provisions and Attaches Slave Storage
-  # Installs IIS Roles
-  # Disables IE Enhanced Secrity
-  # Joins Specified Domain
+#WinRM and PowerShell Provision Functions
   user_data = <<EOF
 <script>
   winrm quickconfig -q & winrm set winrm/config @{MaxTimeoutms="1800000"} & winrm set winrm/config/service @{AllowUnencrypted="true"} & winrm set winrm/config/service/auth @{Basic="true"}
